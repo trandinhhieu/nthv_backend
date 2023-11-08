a99f5d43f46c006c206b1dee0ee66feecd3a52f6
HieuTD <trandinhhieu.dev@gmail.com>
2023-11-08 17:17:50 +0700

    first commit

---
+++ src/main/java/com/hieutd7/noithathanviet/NoithathanvietApplication.java
@@ -0,0 +1,13 @@
+package com.hieutd7.noithathanviet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoithathanvietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoithathanvietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hieutd7/noithathanviet/NoithathanvietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hieutd7.noithathanviet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoithathanvietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
